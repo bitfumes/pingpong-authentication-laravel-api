921a7cbc6d904045e9a98978307b6d6fd81a6bf4
sarthaksavvy <sarthak@bitfumes.com>
2019-07-08 17:02:35 +0530

    5 pingpong setup laravel for jwt auth

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'auth',
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
